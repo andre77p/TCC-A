e38b9fa3a73c4dae21cd9f3c7f17c1e7ee92afe6
andre pereira <andre.p.rafael2018@gmail.com>
2022-10-04 18:14:03 -0300

    tcc

---
+++ src/pages/consultar/index.js
@@ -0,0 +1,17 @@
+import './index.scss';
+
+
+
+export default function Index() {
+    return(
+        <main className='pagina-consulta'>
+               <div class="container-mae">
+
+
+
+
+     </div>
+
+               </main>
+    )
+}
