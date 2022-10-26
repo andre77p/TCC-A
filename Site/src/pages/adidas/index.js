8c48831d661ce7a513042e6944dab58ebdac9231
andre pereira rafael <andre.p.rafael2018@gmail.com>
2022-10-25 23:58:42 -0300

    fish

--- Site/src/pages/adidas/index.js
+++ Site/src/pages/adidas/index.js
@@ -4,16 +4,30 @@ export default function Index(){
     return(
         <main className='pagina-adidas'>
             <nav className='menu'>
-            <h1>Sneakers World</h1>
-            <h1>Nike</h1>
-            <h1>Adidas</h1>
-            <h1>Puma</h1>
-            <h1>Rebook</h1>
-            <h1>New Balance</h1>
-            <input class="pesquisa" type="text" name="nome" placeholder="O que você procura?" autofocus />
-            <img className='carrinho' src='/images/car.png' alt='' />
-            <img className='perfil' src='/images/cont.png' alt='' />
+            <img className='sne' src='/images/snek.png' alt=''/>
+            <div id="linha-horizontal"></div>
+            <div className='text'>
+                <h1>Nike</h1>
+                <h1>Adidas</h1>
+                <h1>Puma</h1>
+                <h1>Rebook</h1>
+                <h1>New Balance</h1>
+                <input class="pesquisa" type="text" name="nome" placeholder="O que você procura?" autofocus />
+                <img className='carrinho' src='/images/car.png' alt='' />
+                <img className='perfil' src='/images/cont.png' alt='' />
+            </div>   
             </nav>
+            <img className='nike' src='/images/nike1-.png' alt=''/>
+
+            <div className='venda'>
+                <div id="tn">
+                    <img src='/images/Heart.png' alt=''/>
+                    <img className='png' src='/images/Nike-T2.png'/>
+                </div>
+                <div id="tn">
+                    <img src='/images/Heart.png' alt=''/>
+                </div>
+            </div>    
         </main>
     )
 }
